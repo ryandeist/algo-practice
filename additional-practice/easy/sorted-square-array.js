5fd420621f224bbceef350fc55628f6282e77159
Ryan Deist <ryandeist@gmail.com>
2025-04-29 12:50:04 -0400

    sorted squar arrays

---
+++ additional-practice/easy/sorted-square-array.js
@@ -0,0 +1,20 @@
+// Sorted Square Array
+
+// Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
+
+// Sample Input
+// array = [1, 2, 3, 5, 6, 8, 9]
+
+// Sample Output
+// [1, 4, 9, 25, 36, 64, 81]
+
+function sortedSquaredArray(array) {
+    // Write your code here.
+    let squares = []
+
+    array.forEach((num) => {
+        squares.push(num * num)
+    })
+
+    return squares.sort((a, b) => a - b)
+}
